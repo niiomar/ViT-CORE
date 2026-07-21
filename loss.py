4344798f7e3d62f9c9d7d87edbd01e5dd3455c64
Jason <163958952+niiomar@users.noreply.github.com>
2026-07-21 06:53:57 +0000

    Remove unnecessary blank lines in loss.py

--- loss.py
+++ loss.py
@@ -1,9 +1,7 @@
 import torch.nn as nn
 
-
 def consistency_loss_mse(view1, view2):
     return nn.MSELoss()(view1, view2)
-
 
 def consistency_loss_cosine(view1, view2):
     cos = nn.CosineSimilarity(dim=1)
